5a2c75d697e431c941fa5abe40ac3ce839a55c08
Nadika <nadika.name@gmail.com>
2023-05-30 12:51:28 +0100

    Remove console log

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
-function formatDate(timestamp){
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if(hours < 10 ){
-        hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if(minutes < 10 ){
-        minutes = `0${minutes}`;
-    }
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
-
-
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-function displayTemperature(response){
-    console.log(response.data);
-    let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    let description = document.querySelector("#description");
-    let humidity = document.querySelector("#humidity");
-    let wind = document.querySelector("#wind");
-    let date = document.querySelector("#date");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
-    cityElement.innerHTML = response.data.name;
-    description.innerHTML = response.data.weather[0].description;
-    humidity.innerHTML = response.data.main.humidity;
-    wind.innerHTML = Math.round(response.data.wind.speed);
-    date.innerHTML = formatDate(response.data.dt * 1000);
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let date = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  date.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "6dceccadd3d7a476d55abb0c59d2a4e7";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
