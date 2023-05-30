502854531ce1fbd97ea4aec728cd6c3ef81dd332
Nadika <nadika.name@gmail.com>
2023-05-30 12:50:30 +0100

    Update the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if(hours < 10 ){
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if(minutes < 10 ){
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+
+
+}
+
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +22,13 @@ function displayTemperature(response){
     let description = document.querySelector("#description");
     let humidity = document.querySelector("#humidity");
     let wind = document.querySelector("#wind");
+    let date = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     description.innerHTML = response.data.weather[0].description;
     humidity.innerHTML = response.data.main.humidity;
     wind.innerHTML = Math.round(response.data.wind.speed);
+    date.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "6dceccadd3d7a476d55abb0c59d2a4e7";
